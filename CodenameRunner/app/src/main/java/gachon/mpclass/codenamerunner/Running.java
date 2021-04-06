0590d788a71b75e7712d8f0815d34cce09304996
ekgp3987 <57945707+ekgp3987@users.noreply.github.com>
2021-04-06 13:54:05 +0900

    Add files via upload

---
+++ CodenameRunner/app/src/main/java/gachon/mpclass/codenamerunner/Running.java
@@ -0,0 +1,14 @@
+package gachon.mpclass.codenamerunner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Running extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running);
+    }
+}
---
+++ CodenameRunner/app/src/main/java/gachon/mpclass/codenamerunner/WriteAgentInfo.java
@@ -0,0 +1,14 @@
+package gachon.mpclass.codenamerunner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteAgentInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_agent_info);
+    }
+}
