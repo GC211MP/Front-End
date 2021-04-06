7f54c36d8cbffc60eca41cc284ab9082d547ec01
ekgp3987 <57945707+ekgp3987@users.noreply.github.com>
2021-04-06 16:41:03 +0900

    add activity
    add activity

---
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/SelectStage.java
@@ -0,0 +1,14 @@
+package gachon.hanul.codenamerun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectStage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_stage);
+    }
+}
---
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/WriteAgentInfo.java
@@ -0,0 +1,14 @@
+package gachon.hanul.codenamerun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteAgentInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_agent_info);
+    }
+}
