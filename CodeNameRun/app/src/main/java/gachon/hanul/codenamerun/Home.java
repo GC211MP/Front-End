256d23d13fb626ea77e34328e6d170a34837f5f2
zlaepek <69634389+zlaepek@users.noreply.github.com>
2021-04-08 12:59:53 +0900

    Stage select (home -> running)
    Stage select (home -> running)
    send stage information to running activity

--- CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Home.java
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Home.java
@@ -2,13 +2,76 @@ package gachon.hanul.codenamerun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    Button TutorialStageButton;
+    Button Stage1Button;
+    Button Stage2Button;
+    Button Stage3Button;
+    Button Stage4Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        /* find view by id */
+        TutorialStageButton = findViewById(R.id.TutorialStageButton);
+        Stage1Button = findViewById(R.id.Stage1Button);
+        Stage2Button = findViewById(R.id.Stage2Button);
+        Stage3Button = findViewById(R.id.Stage3Button);
+        Stage4Button = findViewById(R.id.Stage4Button);
+
+        /* buttons */
+        TutorialStageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent TutorialIntent = new Intent(getApplicationContext(), Running.class);
+                TutorialIntent.putExtra("stageName", "Tutorial");
+                startActivity(TutorialIntent);
+            }
+        });
+
+        Stage1Button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent Stage1Intent = new Intent(getApplicationContext(), Running.class);
+                Stage1Intent.putExtra("stageName", "Stage1");
+                startActivity(Stage1Intent);
+            }
+        });
+
+        Stage2Button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent Stage2Intent = new Intent(getApplicationContext(), Running.class);
+                Stage2Intent.putExtra("stageName", "Stage2");
+                startActivity(Stage2Intent);
+            }
+        });
+
+        Stage3Button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent Stage3Intent= new Intent(getApplicationContext(), Running.class);
+                Stage3Intent.putExtra("stageName", "Stage3");
+                startActivity(Stage3Intent);
+            }
+        });
+
+        Stage4Button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent Stage4Intent= new Intent(getApplicationContext(), Running.class);
+                Stage4Intent.putExtra("stageName", "Stage4");
+                startActivity(Stage4Intent);
+            }
+        });
+
     }
 }
--- CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Running.java
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Running.java
@@ -3,12 +3,46 @@ package gachon.hanul.codenamerun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class Running extends AppCompatActivity {
+
+    /* variables */
+    TextView TopSecret1;
+    TextView TopSecret2;
+    TextView TopSecret3;
+    TextView TopSecret4;
+    TextView TopSecret5;
+    TextView stageNameText;
+    String stageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running);
+
+        /* data from intent */
+        stageName = getIntent().getStringExtra("stageName");
+
+        /* find view by id */
+        TopSecret1 = findViewById(R.id.TopSecret1);
+        TopSecret2 = findViewById(R.id.TopSecret2);
+        TopSecret3 = findViewById(R.id.TopSecret3);
+        TopSecret4 = findViewById(R.id.TopSecret4);
+        TopSecret5 = findViewById(R.id.TopSecret5);
+
+        stageNameText = findViewById(R.id.alertStage);
+
+        /* change values */
+        TopSecret1.setVisibility(View.VISIBLE);
+        TopSecret2.setVisibility(View.INVISIBLE);
+        TopSecret3.setVisibility(View.INVISIBLE);
+        TopSecret4.setVisibility(View.INVISIBLE);
+        TopSecret5.setVisibility(View.INVISIBLE);
+
+        stageNameText.setText(stageName);
     }
+
 }
