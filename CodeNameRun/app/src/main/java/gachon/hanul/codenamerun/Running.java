b9ea7170b321bfb6d5c51499a7fb8d92e2b1f9f6
zlaepek <69634389+zlaepek@users.noreply.github.com>
2021-04-08 20:55:23 +0900

    TTS 음성 테스트
    TTS 음성 성능 테스트
    Running화면에서 홈버튼을 눌렀다 다시 앱을 켜야 소리가 남
    (speak function is in onStart() function)

--- CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Running.java
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Running.java
@@ -3,12 +3,18 @@ package gachon.hanul.codenamerun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.speech.tts.TextToSpeech;
+import static android.speech.tts.TextToSpeech.ERROR;
+
+import java.util.Locale;
 
 public class Running extends AppCompatActivity {
 
+    private TextToSpeech tts;
     /* variables */
     ImageView TopSecret1;
     ImageView TopSecret2;
@@ -43,6 +49,35 @@ public class Running extends AppCompatActivity {
         TopSecret5.setVisibility(View.INVISIBLE);
 
         stageNameText.setText(stageName);
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        tts.speak("나는 어렸을 때부터 정의로운 비밀요원 같은 영화에나 나올 법한 영웅에 관심이 많았다...\n" +
+                "하지만 현실은 그저 평범한 일반인에 지나지 않았다!\n" +
+                "그러던 도중 발견한 구인광고!\n" +
+                "비밀요원을 구한다고?\n" +
+                "누가 비밀요원을 이렇게 전단지로 구하냐 싶냐 만은,\n" +
+                "나의 오랜 꿈을 위해 용기 있게 지원한다! \n" +
+                "어서 오세요! 오늘부터 미션을 수행하시면 됩니다!! 요원 님!!\n" +
+                "네? 저 오늘 취직했는데...\n" +
+                "일단 달려요!!\n" +
+                "으아아!\n",TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+
+
 
 }
