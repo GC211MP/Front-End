b879ac20c6486df025a24b74cc5a9adde315d0ba
ekgp3987 <57945707+ekgp3987@users.noreply.github.com>
2021-04-26 12:18:11 +0900

    create back button in score board

--- CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/ScoreBoard.java
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/ScoreBoard.java
@@ -2,13 +2,25 @@ package gachon.hanul.codenamerun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ScoreBoard extends AppCompatActivity {
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score_board);
+
+        backButton = findViewById(R.id.backButton);
+
+        /*back home*/
+
+        backButton.setOnClickListener(v -> {
+            Intent HomeIntent= new Intent(getApplicationContext(), Home.class);
+            startActivity(HomeIntent);
+        });
     }
 }
