3df642423f612c30d3f25357c5414424c5831d86
zlaepek <69634389+zlaepek@users.noreply.github.com>
2021-05-08 16:53:11 +0900

    list view color change
    list view color change

--- CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/CustomListAdapter.java
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/CustomListAdapter.java
@@ -3,11 +3,18 @@ package gachon.hanul.codenamerun;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 public class CustomListAdapter extends ArrayAdapter{
 
@@ -28,8 +35,22 @@ public class CustomListAdapter extends ArrayAdapter{
 
     @Override
     public View getView(int position, View view, ViewGroup parent) {
+
+        int colorvariable = position;
+        int r, g, b;
+
+        r = 102 + colorvariable * 20;
+        g = 54 + colorvariable * 20;
+        b = 53 + colorvariable * 20;
+
+        int color = Color.rgb(r, g, b);
+
+
         LayoutInflater inflater=context.getLayoutInflater();
         View rowView=inflater.inflate(R.layout.listview_ranking, null,true);
+
+        Drawable sampleDrawable = context.getResources().getDrawable(R.drawable.round, null);
+        sampleDrawable.setTint(color);
 
         //this code gets references to objects in the listview_row.xml file
         TextView nameTextField = (TextView) rowView.findViewById(R.id.name);
@@ -41,20 +62,9 @@ public class CustomListAdapter extends ArrayAdapter{
         recordTextField.setText(recordArray[position]);
         numberTextField.setText(String.valueOf(position + 1));
 
-        int colorvariable;
-        int r, g, b;
-        if (position < 10){
-            colorvariable = position;
-        }
-        else {
-            colorvariable = 10;
-        }
-        r = 218 + colorvariable;
-        g = 116 + colorvariable * 3;
-        b = 111 + colorvariable * 3;
 
-        int color = Color.argb(255, r, g, b);
-        // numberTextField.setBackgroundColor(color);
+
+
 
         return rowView;
 
