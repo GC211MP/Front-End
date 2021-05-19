e19db2235b9523b1f9601c5db12858a921339502
Young2218 <44011517+Young2218@users.noreply.github.com>
2021-05-19 16:42:41 +0900

    컴파일 오류로 빌드파일 다 지움, 에러 수정
    GPS 퍼미션을 회원가입 창으로 옮김
    뒤로가기 누를때 에러를 수정

--- CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Running.java
+++ CodeNameRun/app/src/main/java/gachon/hanul/codenamerun/Running.java
@@ -109,14 +109,14 @@ public class Running extends AppCompatActivity {
         filter.addAction("gachon.hanul.codenamerun.local");
         LocalBroadcastManager.getInstance(this).registerReceiver(localReceiver, filter);
 
-        // gps permission part
-        if (checkLocationServicesStatus()) {
-            Log.d(LOG_IN_RUNNING, "point gps permission");
-            checkRunTimePermission();
-        } else {
-            showDialogForLocationServiceSetting();
-            Log.d(LOG_IN_RUNNING, "point gps permission2");
-        }
+//        // gps permission part
+//        if (checkLocationServicesStatus()) {
+//            Log.d(LOG_IN_RUNNING, "point gps permission");
+//            checkRunTimePermission();
+//        } else {
+//            showDialogForLocationServiceSetting();
+//            Log.d(LOG_IN_RUNNING, "point gps permission2");
+//        }
 
         // set helpGPS and helpMap
         helpGPS = new HelpGPS(this);
@@ -241,7 +241,7 @@ public class Running extends AppCompatActivity {
                     handler.postDelayed(()->backKey= false,1500);
                     return false;
                 } else {
-                    if (bgmPlayer.isPlaying()) {
+                    if (bgmPlayer != null && bgmPlayer.isPlaying()) {
                         bgmPlayer.stop();
                     }
                     // 중첩을 피하기 위해서 다른 activity 로 갈때 quit home activity
@@ -495,93 +495,93 @@ public class Running extends AppCompatActivity {
             }
         }
     }
-    //--------------------------------------------------------------Start <gps permission>----------------------------------------------------------------------
-    /*
-     * TODO: 다혜가 퍼미션 부분에서 뻑난다고 말했음 -> 고쳐야한다
-     * 뻑날때가 있고 뻑 안날때가 있고
-     *
-     * GPS 퍼미션 부분은 아래의 출처에서 가져왔습니다.
-     * Cdde from https://webnautes.tistory.com/1315
-     */
-
-
-    /* if gps service is not available user have to allow to use gps
-     * this function show permission pop up
-     */
-    private void showDialogForLocationServiceSetting() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
-                + "위치 설정을 수정하실래요?");
-        builder.setCancelable(true);
-        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Intent callGPSSettingIntent
-                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
-            }
-        });
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-        builder.create().show();
-    }
-
-    public boolean checkLocationServicesStatus() {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    void checkRunTimePermission() {
-
-        //런타임 퍼미션 처리
-        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
-        int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION);
-
-
-        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
-                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
-
-            // 2. 이미 퍼미션을 가지고 있다면
-            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
-
-
-            // 3.  위치 값을 가져올 수 있음
-
-
-        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
-
-            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
-
-                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
-                Toast.makeText(this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
-                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
-                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE);
-
-
-            } else {
-                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
-                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
-                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE);
-            }
-        }
-    }
-
-
-    //--------------------------------------------------------------end:<gps permission>---------------------------------------------------------------------
+//    //--------------------------------------------------------------Start <gps permission>----------------------------------------------------------------------
+//    /*
+//     * TODO: 다혜가 퍼미션 부분에서 뻑난다고 말했음 -> 고쳐야한다
+//     * 뻑날때가 있고 뻑 안날때가 있고
+//     *
+//     * GPS 퍼미션 부분은 아래의 출처에서 가져왔습니다.
+//     * Cdde from https://webnautes.tistory.com/1315
+//     */
+//
+//
+//    /* if gps service is not available user have to allow to use gps
+//     * this function show permission pop up
+//     */
+//    private void showDialogForLocationServiceSetting() {
+//
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("위치 서비스 비활성화");
+//        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+//                + "위치 설정을 수정하실래요?");
+//        builder.setCancelable(true);
+//        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int id) {
+//                Intent callGPSSettingIntent
+//                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+//            }
+//        });
+//        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int id) {
+//                dialog.cancel();
+//            }
+//        });
+//        builder.create().show();
+//    }
+//
+//    public boolean checkLocationServicesStatus() {
+//        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//
+//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+//                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+//    }
+//
+//    void checkRunTimePermission() {
+//
+//        //런타임 퍼미션 처리
+//        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+//        int hasFineLocationPermission = ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION);
+//        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_COARSE_LOCATION);
+//
+//
+//        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+//                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+//
+//            // 2. 이미 퍼미션을 가지고 있다면
+//            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+//
+//
+//            // 3.  위치 값을 가져올 수 있음
+//
+//
+//        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+//
+//            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+//
+//                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+//                Toast.makeText(this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+//                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+//                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+//                        PERMISSIONS_REQUEST_CODE);
+//
+//
+//            } else {
+//                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+//                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+//                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS,
+//                        PERMISSIONS_REQUEST_CODE);
+//            }
+//        }
+//    }
+//
+//
+//    //--------------------------------------------------------------end:<gps permission>---------------------------------------------------------------------
 
 
 }
